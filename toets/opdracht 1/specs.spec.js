a0abd035fc57943fabbf948448cd10861fe7a2e5
Kamil Agdere <29575@ma-web.nl>
2020-10-20 09:52:02 +0200

    Opdracht 1 afgerond

--- toets/opdracht 1/specs.spec.js
+++ toets/opdracht 1/specs.spec.js
@@ -1,26 +1,27 @@
-describe('Testje 1', function() {
-    it('should have the correct title', function () {
+describe('Opdracht Titel verbeteren', function() {
+    it('De opdracht was om de html titel te verbeteren. Het stond eerst op "Expected Verbeteringen to be Voorbeeld toets" en het enige wat je moest veranderen is dus de titel in de html. Als je dit bericht en uitleg ziet dan weet je dat opdracht is uitgevoerd.', function () {
         expect(document.title).toBe('Voorbeeld toets');
         expect(document.charset).toBe('UTF-8');
     });
 });
 
-describe('Testje 2', function () {
-    it('should test', function () {
-        expect(unitTest.item).toBe('voorbeeld');
-        it('should test', function () {
-            expect(unitTest.nummer).toEqual(3);
-        });
+describe('Opdracht expect functions verbeteren', function () {
+    it('De opdracht hier was om expect(unitTest.item).toBe("voorbeeld"); te verbeteren naar expect(unitTest.item).toBe("item"); als je dat hebt gedaan hoort de fout melding weg te gaan. Als je dit ziet dan is de opdracht correct uitgevoerd.', function () {
+        expect(unitTest.item).toBe('item');
+    });
+    it('De opdracht hier was om de nummer dat in toEqual staat te verbeteren naar 2 inplaats van 3 als dat correct is uitgevoerd zie je geen foutmelding meer. Ook zat dit "it" function in het it function hierboven dat hoor je uit de it functie te halen en plaatsen om een nieuwe regel onder de it function.', function () {
+        expect(unitTest.nummer).toEqual(2);
     });
 });
 
-describe('Testje 3', function () {
-    it('should test', function () {
+describe('Opdracht code uit elkaar halen', function () {
+    it('Deze opdracht was niet zo moeilijk. de opdracht 4 was verwerkt in opdracht 3 en dat moest je simpelweg uit elkaar halen endan zou je dit bericht moeten zien (dit is opdracht 3)', function () {
         expect(unitTest.beschrijving).toBe('Beschrijving');
-        describe('Testje 4', function () {
-            it('should test', function () {
-                expect(unitTest.boodschap).toEqual('Hallo');
-            });
-        });
+    });
+});
+
+describe('Opdracht code uit elkaar halen', function () {
+    it('Deze opdracht was niet zo moeilijk. de opdracht 4 was verwerkt in opdracht 3 en dat moest je simpelweg uit elkaar halen endan zou je dit bericht moeten zien (dit is opdracht 4)', function () {
+        expect(unitTest.boodschap).toEqual('Hallo');
     });
 });
